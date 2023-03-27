f9bdb9e61c14727fa7e623dab8ff2bdf4a3f94cd
JoséSanz <josesanzmartin@hotmail.es>
2023-03-27 16:35:37 +0200

    jose jode

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,22 @@ import { NgModule, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Data } from '../dataJson';
 import { MainserviceService } from '../services/mainservice.service';
+import { ListadaniComponent } from './listadani/listadani.component';
+import { CartadaniComponent } from './cartadani/cartadani.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListadaniComponent,
+    CartadaniComponent
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    ListadaniComponent,
+    CartadaniComponent
   ]
 })
 export class ComponentsModule {
